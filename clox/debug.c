9238392e225f0805bf36fe71d79002deaa91e95a
Daniel957569 <98829797+Daniel957569@users.noreply.github.com>
2022-11-03 21:53:10 +0200

    Update debug.c

--- clox/debug.c
+++ clox/debug.c
@@ -73,6 +73,8 @@ int disassembleInstruction(Chunk *chunk, int offset) {
     return simpleInstruction("OP_PRINT", offset);
   case OP_POP:
     return simpleInstruction("OP_POP", offset);
+  case OP_POPN:
+    return simpleInstruction("OP_POPN", offset);
   case OP_DEFINE_GLOBAL:
     return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
   case OP_GET_GLOBAL:
